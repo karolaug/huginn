d6ddff2d41d642e6e3f927a697e75a7785ad6cf2
Karol Auguštin <karol@augustin.pl>
2015-02-25 18:57:15 +0000

    Roll back to use External ping in PingAgent with localhost

--- app/models/agents/ping_agent.rb
+++ app/models/agents/ping_agent.rb
@@ -25,7 +25,7 @@ module Agents
 
     def check
       for i in 1..options['count'].to_i
-        if Net::Ping::TCP.new("127.0.0.1").ping
+        if Net::Ping::External.new("localhost").ping
           create_event(:payload => {"pingable" => true})
           pingable = true
           break

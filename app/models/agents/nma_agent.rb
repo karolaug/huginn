ddb10a8d0471f48a163243cb284f3bb0fd3fe788
Karol Auguštin <karol@augustin.pl>
2015-03-03 04:15:33 +0000

    Add priority support to NMA Agent

--- app/models/agents/nma_agent.rb
+++ app/models/agents/nma_agent.rb
@@ -15,7 +15,8 @@ module Agents
           'expected_receive_period_in_days' => "2",
           'body' => '{{message}}',
           'title' => '{{subject}}',
-          'apikey' => 'abc'
+          'apikey' => 'abc',
+          'priority' => 'moderate'
       }
     end
 
@@ -24,6 +25,10 @@ module Agents
     end
 
     def validate_options
+      errors.add(:base, 'body is required') unless options['body'].present?
+      errors.add(:base, 'title is required') unless options['title'].present?
+      errors.add(:base, 'apikey is required') unless options['apikey'].present?
+      errors.add(:base, "priority must be set to very_low, moderate, normal, high or emergency") unless %w[very_low moderate normal high emergency].include?(options['priority'])
     end
     
 
@@ -33,7 +38,21 @@ module Agents
         log "Sending notificaction with event #{event.id}"
         NMA.notify do |n|
           n.apikey = options['apikey']
-          n.priority = NMA::Priority::MODERATE
+          if options['priority'] === 'very_low'
+            n.priority = NMA::Priority::VERY_LOW
+          end
+          if options['priority'] === 'moderate'
+            n.priority = NMA::Priority::MODERATE
+          end
+          if options['priority'] === 'normal'
+            n.priority = NMA::Priority::NORMAL
+          end
+          if options['priority'] === 'high'
+            n.priority = NMA::Priority::HIGH
+          end
+          if options['priority'] === 'emergency'
+            n.priority = NMA::Priority::EMERGENCY
+          end
           n.application = "Huginn"
           n.event = interpolated(event)['title']
           n.description = interpolated(event)['body']

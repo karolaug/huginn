1c518645006c21cdaf0b742573555dadfe5556e5
Karol Auguštin <karol@augustin.pl>
2015-03-03 03:25:55 +0000

    NMA Agent

---
+++ app/models/agents/nma_agent.rb
@@ -0,0 +1,33 @@
+require 'ruby-notify-my-android'
+
+module Agents
+  class NMAAgent < Agent
+
+    cannot_be_scheduled!
+    cannot_create_events!
+
+    description <<-MD
+      Sent NMA notifications.
+    MD
+
+    def default_options
+      {
+          'expected_receive_period_in_days' => "2"
+          'body' => '{{message}}'
+      }
+    end
+
+    def receive(incoming_events)
+      incoming_events.each do |event|
+        log "Sending notificaction with event #{event.id}"
+        NMA.notify do |n|
+          n.apikey = options['apikey']
+          n.priority = NMA::Priority::MODERATE
+          n.application = "Huginn"
+          n.event = "Notification"
+          n.description = interpolated(event)['body']
+        end
+      end
+    end
+  end
+end
